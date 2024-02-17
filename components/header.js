b699fa9fc33be4d977730d1c4764b9b1ad43071f
cmd <ada@ads.software>
2024-02-17 11:24:11 -0700

    first commit

---
+++ components/header.js
@@ -0,0 +1,30 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <div class="menu-toggle" id="mobile-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav class="navbar">
+          <a href="index.html">home</a>
+        </nav>
+      </header>
+    `;
+
+    // Add event listener for menu toggle
+    const mobileMenuToggle = this.querySelector('#mobile-menu');
+    const navbar = this.querySelector('.navbar');
+
+    mobileMenuToggle.addEventListener('click', () => {
+      navbar.classList.toggle('active');
+    });
+  }
+}
+
+customElements.define('header-component', Header);
