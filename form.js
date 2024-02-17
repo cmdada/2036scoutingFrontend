7351da090f349b839486b29ddd637903a434fee1
cmd <ada@ads.software>
2024-02-17 11:25:09 -0700

    broken form fixed

--- form.js
+++
@@ -1,7 +0,0 @@
-<form class="form-signin" method="POST" action="URL_OF_REST">
-  <h2 class="form-signin-heading">Team Number in</h2>
-  <input class="form-control" type="text" placeholder="2036">
-  <input class="form-control" type="number" placeholder="Number of breakdowns">
-  <label class="checkbox"></label>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">submit</button>
-</form>
